7ffe5a1154462bcd001f72b2d019ed0c1114f9fb
kent fueconcillo <kentoyfueconcillo@gmail.com>
2017-08-31 15:40:04 +0800

    v1.0.0

--- resttutorial/settings.py
+++ resttutorial/settings.py
@@ -96,23 +96,22 @@ WSGI_APPLICATION = 'resttutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db5l3a4e8tra8h',
-        'USER': 'rtimelketxiiwn',
-        'PASSWORD': '23ddbbaa4191b9d9ba9438bb60c150fb5af30fae3f17689b6a190126d754be3a',
-        'HOST': 'ec2-107-22-167-179.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
-
-#DATABASE_CONFIG = 'postgres://gjesczcdhfbdju:c0bbee38f900a4f4522483984eb8beee73f8e18ee1be2aeb1731380257f0b701@ec2-23-21-197-175.compute-1.amazonaws.com:5432/d5b66atnmcj65v'
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'db5l3a4e8tra8h',
+#         'USER': 'rtimelketxiiwn',
+#         'PASSWORD': '23ddbbaa4191b9d9ba9438bb60c150fb5af30fae3f17689b6a190126d754be3a',
+#         'HOST': 'ec2-107-22-167-179.compute-1.amazonaws.com',
+#         'PORT': '5432',
+#     }
+# }
 # DATABASE_CONFIG = 'postgres://admin:password@127.0.0.1:5432/restpostgre'
-# DATABASE_CONN_MAX_AGE = 0  # No persistent connections.
+DATABASE_CONFIG = 'postgres://gjesczcdhfbdju:c0bbee38f900a4f4522483984eb8beee73f8e18ee1be2aeb1731380257f0b701@ec2-23-21-197-175.compute-1.amazonaws.com:5432/d5b66atnmcj65v'
+DATABASE_CONN_MAX_AGE = 0  # No persistent connections.
 
-# DATABASES = {'default': dj_database_url.config(default=DATABASE_CONFIG)}
-# DATABASES['default']['CONN_MAX_AGE'] = DATABASE_CONN_MAX_AGE
+DATABASES = {'default': dj_database_url.config(default=DATABASE_CONFIG)}
+DATABASES['default']['CONN_MAX_AGE'] = DATABASE_CONN_MAX_AGE
 
 
 # Password validation
